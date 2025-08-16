0ef6fe942344738539477254dca9cbc99dc4c5eb
ArcGabo <pcjhonny13@gmail.com>
2025-08-15 21:16:03 -0500

    Agregando Metadatos

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,12 +28,23 @@ export const metadata: Metadata = {
 		siteName: "IBGroup Blog",
 		locale: "es_ES",
 		type: "website",
+		images: [
+            {
+                url: "https://tusitio.com/ruta/a/tu-imagen.jpg", // Cambia esta URL por la de tu imagen
+                width: 1200,
+                height: 630,
+                alt: "Descripción de la imagen"
+            }
+        ]
 	},
 	twitter: {
 		card: "summary_large_image",
 		title: "Outplacement para Ejecutivos en Perú | IBOutplacement",
 		description: "Blog profesional de IBGroup: artículos, noticias y recursos actualizados.",
 		creator: "@ibgroup",
+		images: [
+            "https://tusitio.com/ruta/a/tu-imagen.jpg" // Cambia esta URL por la de tu imagen
+        ]
 	},
 	icons: {
 		icon: "/favicon.ico",
